1c5b60b71f1d786de21e95305184150c205ec396
belensarome <138254555+belensarome@users.noreply.github.com>
2024-08-02 12:53:06 -0300

    Refactor test user #5

--- test/test_user.py
+++ test/test_user.py
@@ -13,6 +13,17 @@ class UserTestCase(unittest.TestCase):
     YAGNI (You Aren't Gonna Need It) y SOLID (Single Responsibility Principle).
     """
     def setUp(self):
+        self.USERNAME_PRUEBA = 'pabloprats'
+        self.EMAIL_PRUEBA = 'test@test.com'
+        self.PASSWORD_PRUEBA = '123456'
+
+        self.ADDRESS_PRUEBA = 'Address 1234'
+        self.FIRSTNAME_PRUEBA = 'Pablo'
+        self.LASTNAME_PRUEBA = 'Prats'
+        self.PHONE_PRUEBA = '54260123456789'
+        self.CITY_PRUEBA = 'San Rafael'
+        self.COUNTRY_PRUEBA = 'Argentina'
+
         self.app = create_app()
         self.app_context = self.app.app_context()
         self.app_context.push()
@@ -28,69 +39,37 @@ class UserTestCase(unittest.TestCase):
     
     
     def test_user(self):
-        
-        data = UserData()
-        data.firstname = 'Pablo'
-        data.lastname = 'Prats'
-        data.address = 'Address 1234'
-        data.city = 'San Rafael'
-        data.country = 'Argentina'
-        data.phone = '54260123456789'
     
-        user = User(data)
-        user.email = 'test@test.com'
-        user.username = 'pabloprats'
-        user.password = 'Qvv3r7y'
+        user = self.__get_user()
+        # user.save()
 
-        self.assertTrue(user.email, 'test@test.com')
-        self.assertTrue(user.username, 'pabloprats')
-        self.assertTrue(user.password, 'Qvv3r7y')
+        self.assertTrue(user.email, self.EMAIL_PRUEBA)
+        self.assertTrue(user.username, self.USERNAME_PRUEBA)
+        self.assertTrue(user.password, self.PASSWORD_PRUEBA)
         self.assertIsNotNone(user.data)
-        self.assertTrue(user.data.address, 'Address 1234')
-        self.assertTrue(user.data.firstname, 'Pablo')
-        self.assertTrue(user.data.lastname, 'Prats')
-        self.assertTrue(user.data.phone, '54260123456789')   
+        self.assertTrue(user.data.address, self.ADDRESS_PRUEBA)
+        self.assertTrue(user.data.firstname, self.FIRSTNAME_PRUEBA)
+        self.assertTrue(user.data.lastname, self.LASTNAME_PRUEBA)
+        self.assertTrue(user.data.phone, self.PHONE_PRUEBA)   
     
     def test_user_save(self):
         
-        data = UserData()
-        data.firstname = 'Pablo'
-        data.lastname = 'Prats'
-        data.address = 'Address 1234'
-        data.city = 'San Rafael'
-        data.country = 'Argentina'
-        data.phone = '54260123456789'
-    
-        user = User(data)
-        user.email = 'test@test.com'
-        user.username = 'pabloprats'
-        user.password = 'Qvv3r7y'
+        user = self.__get_user()
 
         user_service.save(user)
-        self.assertGreaterEqual(user.id, 1)
-        self.assertTrue(user.email, 'test@test.com')
-        self.assertTrue(user.username, 'pabloprats')
-        self.assertTrue(user.password, 'Qvv3r7y')
+        
+        self.assertTrue(user.email, self.EMAIL_PRUEBA)
+        self.assertTrue(user.username, self.USERNAME_PRUEBA)
+        self.assertTrue(user.password, self.PASSWORD_PRUEBA)
         self.assertIsNotNone(user.data)
-        self.assertTrue(user.data.address, 'Address 1234')
-        self.assertTrue(user.data.firstname, 'Pablo')
-        self.assertTrue(user.data.lastname, 'Prats')
-        self.assertTrue(user.data.phone, '54260123456789')
+        self.assertTrue(user.data.address, self.ADDRESS_PRUEBA)
+        self.assertTrue(user.data.firstname, self.FIRSTNAME_PRUEBA)
+        self.assertTrue(user.data.lastname, self.LASTNAME_PRUEBA)
+        self.assertTrue(user.data.phone, self.PHONE_PRUEBA)  
     
     def test_user_delete(self):
         
-        data = UserData()
-        data.firstname = 'Pablo'
-        data.lastname = 'Prats'
-        data.address = 'Address 1234'
-        data.city = 'San Rafael'
-        data.country = 'Argentina'
-        data.phone = '54260123456789'
-    
-        user = User(data)
-        user.email = 'test@test.com'
-        user.username = 'pabloprats'
-        user.password = 'Qvv3r7y'
+        user = self.__get_user()
 
         user_service.save(user)
 
@@ -100,18 +79,7 @@ class UserTestCase(unittest.TestCase):
     
     def test_user_all(self):
         
-        data = UserData()
-        data.firstname = 'Pablo'
-        data.lastname = 'Prats'
-        data.address = 'Address 1234'
-        data.city = 'San Rafael'
-        data.country = 'Argentina'
-        data.phone = '54260123456789'
-    
-        user = User(data)
-        user.email = 'test@test.com'
-        user.username = 'pabloprats'
-        user.password = 'Qvv3r7y'
+        user = self.__get_user()
         user_service.save(user)
 
         users = user_service.all()
@@ -119,18 +87,7 @@ class UserTestCase(unittest.TestCase):
     
     def test_user_find(self):
         
-        data = UserData()
-        data.firstname = 'Pablo'
-        data.lastname = 'Prats'
-        data.address = 'Address 1234'
-        data.city = 'San Rafael'
-        data.country = 'Argentina'
-        data.phone = '54260123456789'
-    
-        user = User(data)
-        user.email = 'test@test.com'
-        user.username = 'pabloprats'
-        user.password = 'Qvv3r7y'
+        user = self.__get_user()
         user_service.save(user)
 
         user_find = user_service.find(1)
@@ -138,7 +95,22 @@ class UserTestCase(unittest.TestCase):
         self.assertEqual(user_find.id, user.id)
         self.assertEqual(user_find.email, user.email)
 
+    def __get_user(self):
+        data = UserData
+        data = UserData()
+        data.firstname = self.FIRSTNAME_PRUEBA
+        data.lastname = self.LASTNAME_PRUEBA
+        data.phone = self.PHONE_PRUEBA
+        data.address = self.ADDRESS_PRUEBA
+        data.city = self.CITY_PRUEBA
+        data.country = self.COUNTRY_PRUEBA
 
+        user = User(data)
+        user.username = self.USERNAME_PRUEBA
+        user.email = self.EMAIL_PRUEBA
+        user.password = self.PASSWORD_PRUEBA
+
+        return user
 
 if __name__ == '__main__':
     unittest.main()
